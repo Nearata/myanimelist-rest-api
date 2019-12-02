8482091910fea8982ad2c43c568c5d155b980eb0
William Di Cicco <nearata@protonmail.com>
2019-12-02 19:25:51 +0100

    Update validate_route.py

--- mal/middleware/validate_route.py
+++ mal/middleware/validate_route.py
@@ -2,39 +2,68 @@ from re import match
 from falcon import HTTPBadRequest
 from falcon import HTTPMissingParam
 from falcon import HTTPInvalidParam
+from falcon import HTTPNotFound
 from mal.spiders import AnimeSpiders
 
 
 class ValidateRoute:
     def process_request(self, request, response):
-        if match(r"\/(anime)", request.path):
-            anime_regex = r"\/(anime)\/(\d{1,5})\/"
+        anime_route = r"\/(anime)"
+        search_route = r"\/(search)"
+        top_route = r"\/(top)"
+        if match(anime_route, request.path):
+            anime_regex = anime_route + r"\/(\d{1,5})\/"
             anime_routes = f"({'|'.join([i for i in dir(AnimeSpiders) if not i.startswith('__')])})"
             pattern = anime_regex + anime_routes
             if not match(pattern, request.path):
                 raise HTTPBadRequest(
-                    title="The route is invalid or incomplete",
+                    title="The route is invalid or incomplete.",
                     description="Please double check the documentation."
                 )
             if match(anime_regex + r"(reviews|episodes)", request.path):
                 if not match(anime_regex + r"(reviews|episodes)\/(\d+)", request.path):
                     raise HTTPMissingParam("page_number")
-        elif match(r"\/(search\/anime)", request.path):
-            if request.get_param("query", required=True):
-                if len(request.get_param("query")) < 3:
+        elif match(search_route, request.path):
+            search_anime = r"\/anime"
+            if match(search_route + search_anime, request.path):
+                if request.get_param("query", required=True):
+                    if len(request.get_param("query")) < 3:
+                        raise HTTPInvalidParam(
+                            msg="Must be minimum 3 letters.",
+                            param_name="query"
+                        )
+                else:
                     raise HTTPInvalidParam(
-                        msg="Must be minimum 3 letters.",
+                        msg="It cannot be empty.",
                         param_name="query"
                     )
             else:
-                raise HTTPInvalidParam(
-                    msg="It cannot be empty.",
-                    param_name="query"
+                raise HTTPNotFound(
+                    title="Route not found.",
+                    description="Please double check the documentation.",
+                    href="https://github.com/Nearata/myanimelist-rest-api/wiki/Search-Route"
                 )
-        elif match(r"\/(top\/anime)\/(all|airing|upcoming|tv|ova|special|bypopularity|favorite)\/(\d+)", request.path):
-            pass
+        elif match(top_route, request.path):
+            top_anime = r"\/anime"
+            if match(top_route + top_anime, request.path):
+                regex = top_route + top_anime + r"\/(all|airing|upcoming|tv|ova|special|bypopularity|favorite)"
+                if match(regex, request.path):
+                    if not match(regex + r"\/(\d+)", request.path):
+                        raise HTTPMissingParam("page_number")
+                else:
+                    raise HTTPMissingParam(
+                        param_name="type",
+                        href="https://github.com/Nearata/myanimelist-rest-api/wiki/Top-Route"
+                    )
+            else:
+                raise HTTPNotFound(
+                    title="Route not found.",
+                    description="Please double check the documentation.",
+                    href="https://github.com/Nearata/myanimelist-rest-api/wiki/Top-Route"
+                )
         else:
             raise HTTPBadRequest(
-                title="The route is invalid or incomplete",
-                description="Please double check the documentation."
+                title="The route is invalid or incomplete.",
+                description="Please double check the documentation.",
+                href="https://github.com/Nearata/myanimelist-rest-api/wiki"
             )
--- mal/routes/anime_top.py
+++ mal/routes/anime_top.py
@@ -1,10 +1,9 @@
 from json import dumps
-from mal.spiders.anime import AnimeSpiders
+from mal.spiders.anime.top import get_top
 
 
 class AnimeTop:
     def on_get(self, request, response, ttype, page_number):
-        spiders = AnimeSpiders()
-        data = getattr(spiders, "top")(ttype, page_number)
+        data = get_top(ttype, page_number)
         response.content_type = "application/json"
         response.body = dumps(data)
