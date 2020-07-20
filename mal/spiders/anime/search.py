d9b5d77bb78755a8903bf97feb8c9da04d3b8931
William Di Cicco <nearata@protonmail.com>
2020-07-21 01:09:02 +0200

    Improved readability of the `search` code

--- mal/spiders/anime/search.py
+++ mal/spiders/anime/search.py
@@ -4,22 +4,22 @@ from mal.spiders.utils import get_soup
 
 
 class Search:
-    def __init__(self, **kargs) -> None:
-        self.query = kargs["query"]
-        self.type = kargs["type"]
-        self.score = kargs["score"]
-        self.status = kargs["status"]
-        self.producer = kargs["producer"]
-        self.rated = kargs["rated"]
-        self.start_month = kargs["start_month"]
-        self.start_day = kargs["start_day"]
-        self.start_year = kargs["start_year"]
-        self.end_month = kargs["end_month"]
-        self.end_day = kargs["end_day"]
-        self.end_year = kargs["end_year"]
-        self.genres = kargs["genres"]
-        self.genres_exclude = kargs["genres_exclude"]
-        self.columns = kargs["columns"]
+    def __init__(self, **kwargs) -> None:
+        self.query = kwargs["query"]
+        self.type = kwargs["type"]
+        self.score = kwargs["score"]
+        self.status = kwargs["status"]
+        self.producer = kwargs["producer"]
+        self.rated = kwargs["rated"]
+        self.start_month = kwargs["start_month"]
+        self.start_day = kwargs["start_day"]
+        self.start_year = kwargs["start_year"]
+        self.end_month = kwargs["end_month"]
+        self.end_day = kwargs["end_day"]
+        self.end_year = kwargs["end_year"]
+        self.genres = kwargs["genres"]
+        self.genres_exclude = kwargs["genres_exclude"]
+        self.columns = kwargs["columns"]
 
     def get(self):
         params = {
@@ -39,74 +39,53 @@ class Search:
         }
 
         if self.genres:
-            for i in self.genres.split(","):
-                params[f"genre[{i}]"] = i
+            params.update({f"genre[{i}]":i for i in self.genres.split(",")})
 
         columns_lst = []
         if self.columns:
             columns_lst = self.columns.split(",")
-            for i in columns_lst:
-                params[f"c[{i}]"] = i
+            params.update({f"c[{i}]":i for i in self.columns.split(",")})
         else:
-            params[f"c"] = 0
+            params.update({"c":0})
 
-        soup = get_soup("https://myanimelist.net/anime.php", params=params)
-        selector = soup.select_one(".js-categories-seasonal")
+        selector = get_soup("https://myanimelist.net/anime.php", params=params).select_one(".js-categories-seasonal")
+
+        columns_enabled = {}
+        for index, i in enumerate(selector.select_one("tr:first-child").select("td"), 1):
+            columns_enabled.update({"type": index} if "a" in columns_lst and i.find(string="Type") else {})
+            columns_enabled.update({"episodes": index} if "b" in columns_lst and i.find(string="Eps.") else {})
+            columns_enabled.update({"score": index} if "c" in columns_lst and i.find(string="Score") else {})
+            columns_enabled.update({"start_date": index} if "d" in columns_lst and i.find(string="Start Date") else {})
+            columns_enabled.update({"end_date": index} if "e" in columns_lst and i.find(string="End Date") else {})
+            columns_enabled.update({"members": index} if "f" in columns_lst and i.find(string="Members") else {})
+            columns_enabled.update({"rated": index} if "g" in columns_lst and i.find(string="Rated") else {})
 
         results = []
-
-        type_index = None
-        eps_index = None
-        score_index = None
-        s_d_index = None
-        e_d_index = None
-        members_index = None
-        rated_index = None
-
-        for index, i in enumerate(selector.select_one("tr:first-child").select("td"), 1):
-            if "a" in columns_lst and i.find(string="Type"):
-                type_index = index
-            if "b" in columns_lst and i.find(string="Eps."):
-                eps_index = index
-            if "c" in columns_lst and i.find(string="Score"):
-                score_index = index
-            if "d" in columns_lst and i.find(string="Start Date"):
-                s_d_index = index
-            if "e" in columns_lst and i.find(string="End Date"):
-                e_d_index = index
-            if "f" in columns_lst and i.find(string="Members"):
-                members_index = index
-            if "g" in columns_lst and i.find(string="Rated"):
-                rated_index = index
-
         for i in selector.select("tr:not(:first-child)"):
-            anime = {}
-            anime["mal_id"] = int(i.select_one("td:nth-child(2)>div>div").get("rel").replace("a", ""))
-            anime["url"] = i.select_one("td:nth-child(1)>.picSurround>a").get("href")
-            anime["image_url"] = i.select_one("td:nth-child(1)>.picSurround>a>img").get("data-src").replace("r/50x70/", "")
-            anime["title"] = i.select_one("td:nth-child(2)>a>strong").get_text()
-            if type_index:
-                anime["type"] = i.select_one(f"td:nth-child({type_index})").get_text(strip=True)
-            if eps_index:
-                anime["episodes"] = self.__episodes(i.select_one(f"td:nth-child({eps_index})").get_text(strip=True))
-            if score_index:
-                anime["score"] = self.__score(i.select_one(f"td:nth-child({score_index})").get_text(strip=True))
-            if s_d_index:
-                date = i.select_one(f"td:nth-child({s_d_index})").get_text(strip=True).replace("??", "01")
-                if not len(date) <= 1:
-                    anime["start_date"] = str(datetime.strptime(date, "%m-%d-%y").date())
-                else:
-                    anime["start_date"] = None
-            if e_d_index:
-                date = i.select_one(f"td:nth-child({e_d_index})").get_text(strip=True).replace("??", "01")
-                if not len(date) <= 1:
-                    anime["end_date"] = str(datetime.strptime(date, "%m-%d-%y").date())
-                else:
-                    anime["end_date"] = None
-            if members_index:
-                anime["members"] = int(i.select_one(f"td:nth-child({members_index})").get_text(strip=True).replace(",", ""))
-            if rated_index:
-                anime["rated"] = i.select_one(f"td:nth-child({rated_index})").get_text(strip=True)
+            anime = {
+                "mal_id": int(i.select_one("td:nth-child(2)>div>div").get("rel").replace("a", "")),
+                "url": i.select_one("td:nth-child(1)>.picSurround>a").get("href"),
+                "image_url": i.select_one("td:nth-child(1)>.picSurround>a>img").get("data-src").replace("r/50x70/", ""),
+                "title": i.select_one("td:nth-child(2)>a>strong").get_text()
+            }
+
+            for k in columns_enabled.keys():
+                if k == "type":
+                    anime.update({"type": i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True)})
+                if k == "episodes":
+                    anime.update({"episodes": self.__episodes(i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True))})
+                if k == "score":
+                    anime.update({"score": self.__score(i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True))})
+                if k == "start_date":
+                    date = i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True).replace("??", "01")
+                    anime.update({"start_date": str(datetime.strptime(date, "%m-%d-%y").date())} if len(date) > 1 else {})
+                if k == "end_date":
+                    date = i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True).replace("??", "01")
+                    anime.update({"end_date": str(datetime.strptime(date, "%m-%d-%y").date())} if len(date) > 1 else {})
+                if k == "members":
+                    anime.update({"members": int(i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True).replace(",", ""))})
+                if k == "rated":
+                    anime.update({"rated": i.select_one(f"td:nth-child({columns_enabled[k]})").get_text(strip=True)})
 
             results.append(anime)
 
@@ -119,7 +98,6 @@ class Search:
         if regex:
             return int(regex.group())
         return None
-
 
     def __score(self, string):
         regex = match(r"\d\.\d+", string)
