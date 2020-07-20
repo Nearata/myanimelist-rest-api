692606c6eeb1de004863b9979482431660838c8b
William Di Cicco <nearata@protonmail.com>
2020-07-20 22:50:58 +0200

    Update check_url.py
    Actually handle timeout

--- mal/middleware/check_url.py
+++ mal/middleware/check_url.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 from re import search
 from falcon import HTTPError
 from requests import Session
+from requests.exceptions import ReadTimeout
 
 
 class CheckUrl:
@@ -15,9 +16,20 @@ class CheckUrl:
             ]
             for i in regex_list:
                 if i:
-                    res = s.head(f"https://myanimelist.net{i.group()}", timeout=15, headers={
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
-                    })
+                    try:
+                        res = s.head(f"https://myanimelist.net{i.group()}", timeout=15, headers={
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
+                        })
+                    except ReadTimeout:
+                        http_timeout = HTTPStatus(408)
+                        phrase = getattr(http_timeout, "phrase")
+                        description = getattr(http_timeout, "description")
+                        raise HTTPError(
+                            f"408 {phrase}",
+                            phrase,
+                            description,
+                            code=408
+                        )
 
         http_status = HTTPStatus(res.status_code) if res else None
         if res and not str(http_status.value).startswith('2'):
