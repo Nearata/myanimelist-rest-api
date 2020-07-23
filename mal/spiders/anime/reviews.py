07626cc0463a165869f96e2cf12d36401ed7782c
William Di Cicco <nearata@protonmail.com>
2020-07-23 17:44:33 +0200

    Refactoring anime reviews

--- mal/spiders/anime/reviews.py
+++ mal/spiders/anime/reviews.py
@@ -10,51 +10,24 @@ class Reviews:
 
     def get(self):
         selector = get_soup(f"{self.base_url}/anime/{self.mal_id}/_/reviews?p={self.page_number}").select(".js-scrollfix-bottom-rel > .borderDark")
+        reviewer_scores_helper = lambda soup, index: int(soup.select_one(f".textReadability tr:nth-child({index}) > td:last-child").get_text())
         return {
             "reviews": [
                 {
-                    "date": str(
-                        datetime.strptime(
-                            i.select_one(
-                                ".spaceit:first-child > div.mb8:first-child > div:first-child"
-                            ).get_text(),
-                            "%b %d, %Y"
-                        ).date()),
-                    "helpful_count": int(i.select_one(
-                        ".spaceit:first-child > div:nth-child(2) td:last-child > div span"
-                    ).get_text()),
-                    "url": i.select_one(
-                        "div:last-child > div > div > a"
-                    ).get("href"),
+                    "date": str(datetime.strptime(i.select_one(".spaceit:first-child > div.mb8:first-child > div:first-child").get_text(), "%b %d, %Y").date()),
+                    "helpful_count": int(i.select_one(".spaceit:first-child > div:nth-child(2) td:last-child > div span").get_text()),
+                    "url": i.select_one("div:last-child > div > div > a").get("href"),
                     "reviewer": {
-                        "profile_url": i.select_one(
-                            ".spaceit:first-child > div:last-child .picSurround > a"
-                        ).get("href"),
-                        "image_url": i.select_one(
-                            ".spaceit:first-child > div:last-child .picSurround > a > img"
-                        ).get("data-src"),
-                        "username": i.select_one(
-                            ".spaceit:first-child > div:last-child td:last-child > a"
-                        ).get_text(),
+                        "profile_url": i.select_one(".spaceit:first-child > div:last-child .picSurround > a").get("href"),
+                        "image_url": i.select_one(".spaceit:first-child > div:last-child .picSurround > a > img").get("data-src"),
+                        "username": i.select_one(".spaceit:first-child > div:last-child td:last-child > a").get_text(),
                         "scores": {
-                            "overall": int(i.select_one(
-                                ".textReadability tr:nth-child(1) > td:last-child > strong"
-                            ).get_text()),
-                            "story": int(i.select_one(
-                                ".textReadability tr:nth-child(2) > td:last-child"
-                            ).get_text()),
-                            "animation": int(i.select_one(
-                                ".textReadability tr:nth-child(3) > td:last-child"
-                            ).get_text()),
-                            "sound": int(i.select_one(
-                                ".textReadability tr:nth-child(4) > td:last-child"
-                            ).get_text()),
-                            "character": int(i.select_one(
-                                ".textReadability tr:nth-child(5) > td:last-child"
-                            ).get_text()),
-                            "enjoyment": int(i.select_one(
-                                ".textReadability tr:nth-child(6) > td:last-child"
-                            ).get_text())
+                            "overall": reviewer_scores_helper(i, 1),
+                            "story": reviewer_scores_helper(i, 2),
+                            "animation": reviewer_scores_helper(i, 3),
+                            "sound": reviewer_scores_helper(i, 4),
+                            "character": reviewer_scores_helper(i, 5),
+                            "enjoyment": reviewer_scores_helper(i, 6)
                         }
                     }
                 } for i in selector
