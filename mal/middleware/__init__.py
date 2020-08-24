847bb7087691607c61d7ea7d48871918b75e7765
William Di Cicco <nearata@protonmail.com>
2020-08-25 00:51:27 +0200

    Removed last Check Url import

--- mal/middleware/__init__.py
+++ mal/middleware/__init__.py
@@ -1,5 +1,4 @@
 from mal.middleware.cache import CacheMiddleware
 from mal.middleware.mal_checker import MalChecker
-from mal.middleware.check_url import CheckUrlMiddleware
 from mal.middleware.require_json import RequireJsonMiddleware
 from mal.middleware.validate_route import ValidateRouteMiddleware
