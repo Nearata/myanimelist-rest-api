37bdb803772af3543ed2226352e71b73c6e7667c
Nearata <nearata@protonmail.com>
2021-04-30 15:02:27 +0200

    fix tests

--- tests/anime/__init__.py
+++ tests/anime/__init__.py
@@ -1,7 +1,8 @@
 from requests import Session
 
 from mal.main import create_app
-
+from mal.cache import CacheUtil
 
 app = create_app()
 app.state.session = Session()
+app.state.cache = CacheUtil()
