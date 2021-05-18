5d9bb03f382956794200043eca78a1a07a2dd7e5
Nearata <nearata@protonmail.com>
2021-05-18 21:07:24 +0200

    Improved DisabledRoutesMiddleware
    * other than checking if there are routes disabled, this middleware will also check if a route exists in the application

--- mal/middlewares/disabled_routes.py
+++ mal/middlewares/disabled_routes.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 from typing import Any
 
 from fastapi import Request
+from fastapi.routing import APIRoute
 from starlette.responses import JSONResponse
 
 from mal.config import DISABLED_ROUTES
@@ -9,13 +10,13 @@ from mal.config import DISABLED_ROUTES
 
 class DisabledRoutesMiddleware:
     async def __call__(self, request: Request, call_next: Any) -> Any:
-        path: str = request.scope["path"]
+        path: str = request.scope.get("path", "")
+        routes: list[str] = [i.path for i in request.app.routes]
 
-        http_status = HTTPStatus(404)
-        phrase = http_status.phrase
-        description = http_status.description
-
-        if any(i in path for i in DISABLED_ROUTES):
+        if path not in routes or path in DISABLED_ROUTES:
+            http_status = HTTPStatus(404)
+            phrase = http_status.phrase
+            description = http_status.description
             return JSONResponse(
                 {"title": f"404 {phrase}", "description": description}, 404
             )
