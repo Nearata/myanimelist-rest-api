70d36f53cb2f42a6b3378ad3d475e49b75aed431
Nearata <nearata@protonmail.com>
2022-05-13 17:13:12 +0200

    fix anime details

--- mal/parsers/anime/details.py
+++ mal/parsers/anime/details.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from re import match
-from typing import Optional, Union
+from re import findall, match, search
+from typing import Any, Optional, Union
 
 from bs4 import BeautifulSoup
 
+from ...const import MAL_URL
 from ._helpers import AnimeHelpers
 
 
@@ -15,138 +16,324 @@ class Details:
         self.base_url = base_url
 
     def __call__(self) -> dict:
-        anime_title: str = self.soup.select_one("h1.title-name").get_text()
-        image = self.__get_image(anime_title)
-
-        return {
-            "data": {
-                "title": anime_title,
-                "image": image,
-                "trailer": AnimeHelpers.trailer_helper(self.soup),
-                "synopsis": AnimeHelpers.synopsis_helper(self.soup),
-                "background": AnimeHelpers.background_helper(self.soup),
-                "alternativeTitles": {
-                    "english": self.__alternative_titles_helper("English:"),
-                    "japanese": self.__alternative_titles_helper("Japanese:"),
-                    "synonyms": AnimeHelpers.synonyms_helper(self.soup),
-                },
-                "information": {
-                    "type": self.soup.find("span", string="Type:")
-                    .find_next_sibling("a")
-                    .get_text(),
-                    "episodes": AnimeHelpers.episodes_helper(self.soup),
-                    "status": self.soup.find(
-                        "span", string="Status:"
-                    ).next_sibling.strip(),
-                    "aired": {
-                        "from": self.__information_aired_helper("from"),
-                        "to": self.__information_aired_helper("to"),
-                    },
-                    "premiered": AnimeHelpers.premiered_helper(self.soup),
-                    "producers": AnimeHelpers.producers_helper(
-                        self.soup, self.none_found, self.base_url
-                    ),
-                    "licensors": AnimeHelpers.licensors_helper(
-                        self.soup, self.none_found, self.base_url
-                    ),
-                    "studios": AnimeHelpers.studios_helper(
-                        self.soup, self.none_found, self.base_url
-                    ),
-                    "source": self.__alternative_titles_helper("Source:"),
-                    "genres": AnimeHelpers.genres_helper(self.soup),
-                    "duration": AnimeHelpers.duration_helper(self.soup),
-                    "rating": AnimeHelpers.rating_helper(self.soup),
-                },
-                "statistics": {
-                    "score": AnimeHelpers.score_helper(self.soup),
-                    "ranked": self.__statistics_helper("Ranked:", "#"),
-                    "popularity": self.__statistics_helper("Popularity:", "#"),
-                    "members": self.__statistics_helper("Members:"),
-                    "favorites": self.__statistics_helper("Favorites:"),
-                },
-                "relatedAnime": {
-                    "adaptation": self.__related_anime_helper("Adaptation:"),
-                    "sideStory": self.__related_anime_helper("Side story:"),
-                    "summary": self.__related_anime_helper("Summary:"),
-                    "spinOff": self.__related_anime_helper("Spin-off:"),
-                    "other": self.__related_anime_helper("Other:"),
-                    "prequel": self.__related_anime_helper("Prequel:"),
-                    "character": self.__related_anime_helper("Character:"),
-                    "sequel": self.__related_anime_helper("Sequel:"),
-                },
-                "openingTheme": self.__theme_song_helper("opnening"),
-                "endingTheme": self.__theme_song_helper("ending"),
-            }
+        details: dict[str, Any] = {
+            "title": None,
+            "pictureUrl": None,
+            "synopsis": None,
+            "background": None,
+            "titleJapanese": None,
+            "titleEnglish": None,
+            "titleSynonyms": [],
+            "type": None,
+            "episodes": None,
+            "status": None,
+            "aired": {"from": None, "to": None},
+            "premiered": None,
+            "producers": [],
+            "licensors": [],
+            "studios": [],
+            "source": None,
+            "genres": [],
+            "themes": [],
+            "duration": None,
+            "rating": None,
+            "score": None,
+            "ranked": None,
+            "popularity": None,
+            "members": None,
+            "favorites": None,
+            "externalLinks": [],
+            "streamingPlatforms": [],
+            "sequel": None,
+            "prequel": None,
+            "alternativeSetting": None,
+            "alternativeVersion": None,
+            "sideStory": None,
+            "summary": None,
+            "fullStory": None,
+            "parentStory": None,
+            "spinOff": None,
+            "adaptation": None,
+            "character": None,
+            "other": None,
+            "openingTheme": [],
+            "endingTheme": [],
+            "trailerUrl": [],
         }
 
-    def __related_anime_helper(self, string: str) -> list:
-        related_anime = self.soup.find("td", string=string)
-        return (
-            [
-                {
-                    "title": i.get_text(),
-                    "type": i.get("href").split("/")[1],
-                    "malId": int(i.get("href").split("/")[2]),
-                }
-                for i in related_anime.next_sibling.select("a")
-            ]
-            if related_anime
-            else []
-        )
+        details.update({"title": self.soup.select_one("h1").get_text().strip()})
 
-    def __theme_song_helper(self, string: str) -> list:
-        theme_song = self.soup.select(f"div.{string} > span.theme-song")
-        return (
-            [{"title": str(i.get_text(strip=True))} for i in theme_song]
-            if theme_song
-            else []
-        )
+        picture = self.soup.find("img", attrs={"alt": details["title"]})
+        if picture:
+            details.update({"pictureUrl": picture.get("data-src")})
 
-    def __statistics_helper(self, field: str, replace: str = ",") -> Union[int, None]:
-        try:
-            return int(
-                self.soup.find("span", string=field)
-                .next_sibling.replace(replace, "")
-                .strip()
+        title_japanese = self.soup.find("span", string="Japanese:")
+        if title_japanese and (sibling := title_japanese.next_sibling):
+            details.update({"titleJapanese": sibling.get_text().strip()})
+
+        title_english = self.soup.find("span", string="English:")
+        if title_english and (sibling := title_english.next_sibling):
+            details.update({"titleEnglish": sibling.get_text().strip()})
+
+        title_synonyms = self.soup.find("span", string="Synonyms:")
+        if title_synonyms and (sibling := title_synonyms.next_sibling):
+            details.update(
+                {"titleSynonyms": [i.strip() for i in sibling.get_text().split(",")]}
             )
-        except ValueError:
-            return None
 
-    def __alternative_titles_helper(self, field_name: str) -> Union[str, None]:
-        field = self.soup.find("span", string=field_name)
-        return str(field.next_sibling.strip()) if field else None
+        _type = self.soup.find("span", string="Type:")
+        if _type and (sibling := _type.find_next_sibling("a")):
+            details.update({"type": sibling.get_text().strip()})
 
-    def __information_aired_helper(self, index: str) -> Union[str, None]:
-        aired: str = self.soup.find("span", string="Aired:").next_sibling.strip()
+        episodes = self.soup.find("span", string="Episodes:")
+        if (
+            episodes
+            and (sibling := episodes.next_sibling)
+            and (found := match(r"\d+", sibling.get_text().strip()))
+        ):
+            details.update({"episodes": int(found.group())})
 
-        found = match(
-            r"(?P<month1>\w+)\s(?P<day1>\d+),\s(?P<year1>\d+)\sto\s(?P<month2>\w+)\s(?P<day2>\d+),\s(?P<year2>\d+)",
-            aired,
+        status = self.soup.find("span", string="Status:")
+        if status and (sibling := status.next_sibling):
+            details.update({"status": sibling.get_text().strip()})
+
+        aired = self.soup.find("span", string="Aired:")
+        if aired and (sibling := aired.next_sibling):
+            details.update({"aired": self.__get_aired(sibling.get_text().strip())})
+
+        premiered = self.soup.find("span", string="Premiered:")
+        if premiered and (sibling := premiered.find_next_sibling("a")):
+            details.update({"premiered": sibling.get_text().strip()})
+
+        producers = self.soup.find("span", string="Producers:")
+        if (
+            producers
+            and (sibling := producers.find_next_siblings("a"))
+            and sibling[0].get("href").startswith("/anime/producer")
+        ):
+            details.update({"producers": [i.get_text().strip() for i in sibling]})
+
+        licensors = self.soup.find("span", string="Licensors:")
+        if (
+            licensors
+            and (sibling := licensors.find_next_siblings("a"))
+            and sibling[0].get("href").startswith("/anime/producer")
+        ):
+            details.update({"licensors": [i.get_text().strip() for i in sibling]})
+
+        studios = self.soup.find("span", string="Studios:")
+        if (
+            studios
+            and (sibling := studios.find_next_siblings("a"))
+            and sibling[0].get("href").startswith("/anime/producer")
+        ):
+            details.update({"studios": [i.get_text().strip() for i in sibling]})
+
+        source = self.soup.find("span", string="Source:")
+        if source and (sibling := source.next_sibling):
+            details.update({"source": sibling.get_text().strip()})
+
+        genres = self.soup.find("span", string="Genres:")
+        if genres and (sibling := genres.find_next_siblings("a")):
+            details.update({"genres": [i.get_text().strip() for i in sibling]})
+
+        themes = self.soup.find("span", string="Themes:")
+        if themes and (sibling := themes.find_next_siblings("a")):
+            details.update({"themes": [i.get_text().strip() for i in sibling]})
+
+        duration = self.soup.find("span", string="Duration:")
+        if duration and (sibling := duration.next_sibling):
+            details.update(
+                {"duration": self.__get_duration(sibling.get_text().strip())}
+            )
+
+        rating = self.soup.find("span", string="Rating:")
+        if rating and (sibling := rating.next_sibling):
+            details.update({"rating": sibling.get_text().strip()})
+
+        score = self.soup.find("span", attrs={"itemprop": "ratingValue"})
+        if score and (found := match(r"\d\.\d+", score.get_text().strip())):
+            details.update({"score": float(found.group())})
+
+        trailer = self.soup.find("div", attrs={"class": "video-promotion"})
+        if trailer and (child := trailer.find("a")):
+            details.update({"trailerUrl": child.get("href")})
+
+        ranked = self.soup.find("span", string="Ranked:")
+        if ranked and (sibling := ranked.next_sibling) and "#" in sibling.get_text():
+            details.update({"ranked": sibling.get_text().strip()})
+
+        popularity = self.soup.find("span", string="Popularity:")
+        if (
+            popularity
+            and (sibling := popularity.next_sibling)
+            and "#" in sibling.get_text()
+        ):
+            details.update({"popularity": sibling.get_text().strip()})
+
+        members = self.soup.find("span", string="Members:")
+        if (
+            members
+            and (sibling := members.next_sibling)
+            and search(r"\d", sibling.get_text())
+        ):
+            details.update(
+                {"members": int("".join(sibling.get_text().strip().split(",")))}
+            )
+
+        favorites = self.soup.find("span", string="Favorites:")
+        if (
+            favorites
+            and (sibling := favorites.next_sibling)
+            and search(r"\d", sibling.get_text())
+        ):
+            details.update(
+                {"favorites": int("".join(sibling.get_text().strip().split(",")))}
+            )
+
+        external_links = self.soup.find("h2", string="External Links")
+        if external_links and (sibling := external_links.find_next_sibling("div")):
+            details.update(
+                {
+                    "externalLinks": [
+                        {"name": i.get_text().strip(), "url": i.get("href")}
+                        for i in sibling.find_all("a")
+                    ]
+                }
+            )
+
+        if streaming_platforms := self.soup.find_all(
+            "a", attrs={"class": "broadcast-item"}
+        ):
+            details.update(
+                {
+                    "streamingPlatforms": [
+                        {"name": i.get_text().strip(), "url": i.get("href")}
+                        for i in streaming_platforms
+                    ]
+                }
+            )
+
+        synopsis = self.soup.find("p", attrs={"itemprop": "description"})
+        if synopsis and "no synopsis" not in synopsis.get_text().lower():
+            details.update({"synopsis": synopsis.get_text().strip()})
+
+        background = self.soup.find("p", attrs={"itemprop": "description"})
+        if (
+            background
+            and (parent := background.parent)
+            and "no background" not in parent.get_text().lower()
+        ):
+            for i in parent.select("div, p"):
+                i.decompose()
+
+            details.update({"background": parent.get_text().strip()})
+
+        related_anime = self.soup.find(
+            "table", attrs={"class": "anime_detail_related_anime"}
         )
+        if related_anime:
+            for i in related_anime.select("tr"):
+                related1 = i.select_one("td:first-child")
+                related2 = i.select_one("td:last-child")
+
+                if related1 and related2:
+                    related1_text = (
+                        related1.get_text()
+                        .strip()
+                        .removesuffix(":")
+                        .lower()
+                        .replace("-", " ")
+                        .split(" ")
+                    )
+                    related1_camel = related1_text[0] + "".join(
+                        i.capitalize() for i in related1_text[1:]
+                    )
+                    details.update(
+                        {
+                            f"{related1_camel}": [
+                                {
+                                    "name": related_item.get_text().strip(),
+                                    "url": MAL_URL + related_item.get("href"),
+                                    "id": int(id_found.group())
+                                    if (
+                                        id_found := search(
+                                            r"\d+", related_item.get("href")
+                                        )
+                                    )
+                                    else None,
+                                }
+                                for related_item in related2.find_all("a")
+                            ]
+                        }
+                    )
+
+        opening_theme = self.soup.find("h2", string="Opening Theme")
+        if opening_theme and (parent := opening_theme.parent) and (sibling := parent.next_sibling):
+            songs = []
+            for i in sibling.find_all_next("tr"):
+                s = {}
+
+                if found := i.find("span", attrs={"class": "theme-song-title"}):
+                    s.update({"title": found.get_text().strip().replace('"', "")})
+
+                if found := i.find("span", attrs={"class": "theme-song-artist"}):
+                    s.update({"name": found.get_text().strip().replace("by ", "")})
+
+                if (
+                    found := i.find("span", attrs={"class": "theme-song-episode"})
+                ) and (found_episodes := findall(r"\d+", found.get_text().strip())):
+                    s.update({"episodes": "-".join(found_episodes)})
+
+                if s:
+                    songs.append(s)
+
+            details.update({"openingTheme": songs})
+
+        ending_theme = self.soup.find("h2", string="Ending Theme")
+        if ending_theme and (parent := ending_theme.parent) and (sibling := parent.next_sibling):
+            songs = []
+            for i in sibling.find_all_next("tr"):
+                s = {}
+
+                if found := i.find("span", attrs={"class": "theme-song-title"}):
+                    s.update({"title": found.get_text().strip().replace('"', "")})
+
+                if found := i.find("span", attrs={"class": "theme-song-artist"}):
+                    s.update({"name": found.get_text().strip().replace("by ", "")})
+
+                if (
+                    found := i.find("span", attrs={"class": "theme-song-episode"})
+                ) and (found_episodes := findall(r"\d+", found.get_text().strip())):
+                    s.update({"episodes": "-".join(found_episodes)})
+
+                if s:
+                    songs.append(s)
+
+            details.update({"endingTheme": songs})
+
+        return {"data": dict(sorted(details.items()))}
+
+    def __get_aired(self, string: str) -> dict[str, Any]:
+        found = findall(r"(\w+)\s(\d{1,2}),\s(\d{4,})", string)
+
+        if not found:
+            return {"from": None, "to": None}
+
+        fmt = lambda i: str(datetime.strptime(i, "%b %d %Y").date())
+
+        if len(found) == 2:
+            return {"from": fmt(" ".join(found[0])), "to": fmt(" ".join(found[1]))}
+
+        return {"from": fmt(" ".join(found[0])), "to": None}
+
+    def __get_duration(self, string: str) -> Optional[str]:
+        found = findall(r"\d+", string)
 
         if not found:
             return None
 
-        month1 = found.group("month1")
-        day1 = found.group("day1")
-        year1 = found.group("year1")
-        date_from = f"{month1} {day1}, {year1}"
+        if len(found) == 2:
+            return f"{found[0]}h{found[1]}m"
 
-        month2 = found.group("month2")
-        day2 = found.group("day2")
-        year2 = found.group("year2")
-        date_to = f"{month2} {day2}, {year2}"
-
-        return str(
-            datetime.strptime(
-                date_from if index == "from" else date_to, "%b %d, %Y"
-            ).date()
-        )
-
-    def __get_image(self, title: str) -> Optional[str]:
-        image = self.soup.find("img", alt=title)
-
-        if image is None:
-            return None
-
-        return image.get("data-src")
+        return f"{found[0]}m"
--- tests/anime/test_details.py
+++ tests/anime/test_details.py
@@ -11,59 +11,33 @@ async def test_details(client: AsyncClient) -> None:
     details = response.json()["data"]
 
     assert isinstance(details["title"], str)
-    assert isinstance(details["image"], str)
-    assert isinstance(details["trailer"], str)
+    assert isinstance(details["pictureUrl"], str)
+    assert isinstance(details["trailerUrl"], str)
     assert isinstance(details["synopsis"], str)
     assert isinstance(details["background"], str)
-
-    alternative_titles = details["alternativeTitles"]
-    assert isinstance(alternative_titles["english"], str)
-    assert isinstance(alternative_titles["japanese"], str)
-    assert isinstance(alternative_titles["synonyms"], list)
-
-    information = details["information"]
-    assert isinstance(information["type"], str)
-    assert isinstance(information["episodes"], int)
-    assert isinstance(information["status"], str)
-    assert isinstance(information["aired"]["from"], str)
-    assert isinstance(information["aired"]["to"], str)
-    assert isinstance(information["premiered"], str)
-
-    producer = information["producers"][0]
-    assert isinstance(producer["name"], str)
-    assert isinstance(producer["url"], str)
-    assert isinstance(producer["malId"], int)
-
-    licensor = information["licensors"][0]
-    assert isinstance(licensor["name"], str)
-    assert isinstance(licensor["url"], str)
-    assert isinstance(licensor["malId"], int)
-
-    studio = information["studios"][0]
-    assert isinstance(studio["name"], str)
-    assert isinstance(studio["url"], str)
-    assert isinstance(studio["malId"], int)
-
-    assert isinstance(information["source"], str)
-
-    genre = information["genres"][0]
-    assert isinstance(genre["name"], str)
-    assert isinstance(genre["malId"], int)
-
-    assert isinstance(information["duration"], int)
-    assert isinstance(information["rating"], str)
-
-    statistics = details["statistics"]
-    assert isinstance(statistics["score"], float)
-    assert isinstance(statistics["ranked"], int)
-    assert isinstance(statistics["popularity"], int)
-    assert isinstance(statistics["members"], int)
-    assert isinstance(statistics["favorites"], int)
-
-    adaptation = details["relatedAnime"]["adaptation"][0]
-    assert isinstance(adaptation["title"], str)
-    assert isinstance(adaptation["type"], str)
-    assert isinstance(adaptation["malId"], int)
-
-    assert isinstance(details["openingTheme"][0]["title"], str)
-    assert isinstance(details["endingTheme"][0]["title"], str)
+    assert isinstance(details["titleEnglish"], str)
+    assert isinstance(details["titleJapanese"], str)
+    assert isinstance(details["titleSynonyms"], list)
+    assert isinstance(details["type"], str)
+    assert isinstance(details["episodes"], int)
+    assert isinstance(details["status"], str)
+    assert isinstance(details["aired"]["from"], str)
+    assert isinstance(details["aired"]["to"], str)
+    assert isinstance(details["premiered"], str)
+    assert len(details["producers"]) > 0
+    assert len(details["licensors"]) > 0
+    assert len(details["studios"]) > 0
+    assert isinstance(details["source"], str)
+    assert len(details["genres"]) > 0
+    assert isinstance(details["duration"], str)
+    assert isinstance(details["rating"], str)
+    assert isinstance(details["score"], float)
+    assert isinstance(details["ranked"], str)
+    assert isinstance(details["popularity"], str)
+    assert isinstance(details["members"], int)
+    assert isinstance(details["favorites"], int)
+    assert len(details["adaptation"]) > 0
+    assert len(details["sideStory"]) > 0
+    assert len(details["summary"]) > 0
+    assert len(details["openingTheme"]) > 0
+    assert len(details["endingTheme"]) > 0
